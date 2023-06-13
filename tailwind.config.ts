d67f6ad060a588c181fed9f92de334dcd5f4626a
Hayden Hanson <hhanson2@gmu.edu>
2023-06-13 11:15:31 -0400

    Theme Added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,4 +6,21 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        gmuTheme: {
+          primary: "#006940",
+          secondary: "#ffcc33",
+          accent: "#27825F",
+          neutral: "#001C00",
+          "base-100": "#003621",
+          info: "#3358FF",
+          success: "#00B570",
+          warning: "#E8A323",
+          error: "#B51F00",
+        },
+      },
+    ],
+  },
 } satisfies Config;
